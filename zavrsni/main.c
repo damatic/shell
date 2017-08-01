6020014b7d307f31904f3983a81458c3e416ad1c
Danijel <d.matic91@gmail.com>
2017-08-01 21:30:58 +0200

    Update main.c

--- zavrsni/main.c
+++ zavrsni/main.c
@@ -5,13 +5,15 @@
 #include <linux/limits.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 #define BUFFER_LENGTH 1024
 
 int main()
 {	
 	pid_t pid;
-	char program_path[PATH_MAX] = "/home/stingy/Desktop/zavrsni/commands/";
+	char path[256] = "/home/stingy/Desktop/zavrsni/commands/";
+	char program_path[PATH_MAX];
 	char line[BUFFER_LENGTH];
 	char* argv[256];
 	char* token;
@@ -19,8 +21,8 @@ int main()
 	int argc = 0;
 	size_t length;
 	
-	
 	while(1){
+		strcpy(program_path, path);
 	
 		printf("test >> ");
 
@@ -44,12 +46,14 @@ int main()
 			token = strtok(line, " ");
 			while(token != NULL && i < 256){
 				argv[i] = token;
-				token = strtok(NULL, " ");
+				token = strtok(NULL, " \n\t()<>|&;");
 				i++;
 			}
 			
 			argv[i] == NULL;
+			printf("Prije: %s\n", program_path);
 			strcat(program_path, argv[0]);
+			printf("Poslije: %s\n", program_path);
 			
 			//printf("%s\n", program_path); // ispis putanje programa
 			
@@ -61,25 +65,29 @@ int main()
 			
 			pid = fork();
 			if(pid == 0){ // child process
-				execvp(program_path, argv);
-				fprintf(stderr, "Child process could not do execvp\n");
+				
+				if (execvp(program_path, argv) == -1)
+                {
+                    printf("ERRNO: %s\n", strerror(errno));
+                    _exit(1);
+                }
 			}else{ // parrent process
 				wait(NULL);
-		    	printf("Child exited\n");
-		    	program_path[0] = 0;
+		    	
+		    	//program_path[PATH_MAX] = "/home/stingy/Desktop/zavrsni/commands/";
+		    	// postavljanje svih varijabli na pocetno stanje
 		    	argv[0] = 0;
-		    	line[0] = 0;
+		    	line[0] = '\0';
 				length = 0;
 				argc = 0;
 				i = 0;
+				//printf("Child exited\n");
 			}
 		}
 		//printf("%s\n", program_path);
 	}
-    
 	return EXIT_SUCCESS;
 }
-
 
 
 
