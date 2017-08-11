75e58a9f6e791f089e90df33e3a2179ff645b75a
Danijel <d.matic91@gmail.com>
2017-08-11 03:40:04 +0200

    Update main.c

--- zavrsni/main.c
+++ zavrsni/main.c
@@ -6,8 +6,28 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include "builtin.h"
+
 
 #define BUFFER_LENGTH 1024 // velicina buffera
+
+/*
+int initialize_shell()
+{
+	char path[256] = "/home/stingy/Desktop/zavrsni/commands/";
+	char program_path[PATH_MAX];
+	char line[BUFFER_LENGTH];
+	char* argv[256];
+	
+	void (*fbuiltin_ptr_arr[])(char**) = {
+		&exit_shell, 
+		&echo, cdir, 
+		&pwd, 
+		&clear_terminal
+	};
+}
+*/
+
 
 int main()
 {	
@@ -21,12 +41,20 @@ int main()
 	int argc = 0;
 	size_t length;
 	
-	//dodati cd, clear, pwd...
+	char *builtin_str[] = {
+  		"cd",
+  		"help",
+  		"exit"
+	};
+
+	
+	// dodati cd, clear, pwd...
 	while(1){
 		strcpy(program_path, path);
 	
 		printf("test >> ");
-
+		pwd(); // probno path
+		
 		if (fgets(line, BUFFER_LENGTH, stdin) == 0){ // u slucaju da je ^D End-Of-File
 			return 1;
 		}
@@ -41,7 +69,11 @@ int main()
 			}		
 		
 	  		if(strcmp(line, "exit") == 0){ // probno exit
-		    	return 1;
+		    	exitshell();
+			}
+			
+			if(strcmp(line, "clear") == 0){ // probno clear
+		    	clear_terminal();
 			}
 
 			token = strtok(line, " \n\t()<>|&;");
@@ -50,6 +82,10 @@ int main()
 				token = strtok(NULL, " \n\t()<>|&;"); // " \n\t()<>|&;" znakovi koje "ignorira"
 				i++;
 			}
+			
+			if(strcmp(argv[0], "cdir") == 0){ // probno change directory
+		    	cdir(argv[1]);
+			}else
 			
 			argc = i;
 			argv[i] == NULL; // potrebno zbog execvp, da se zna gdje je kraj
@@ -61,7 +97,7 @@ int main()
 				if (execvp(program_path, argv) == -1)
                 {
                     printf("ERRNO: %s\n", strerror(errno));
-                    _exit(1);
+                    _exit(0);
                 }
 			}else{ // parrent process
 				wait(NULL);
